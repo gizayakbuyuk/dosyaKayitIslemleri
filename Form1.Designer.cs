5ca113a44829a677f4316a51a554b28fe1296ebd
gizayakbuyuk <gizaykata@gmail.com>
2016-03-17 19:19:34 +0200

    Added files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Dosya_Kayıt_İşlemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxAd = new System.Windows.Forms.TextBox();
+            this.textBoxSoyad = new System.Windows.Forms.TextBox();
+            this.textBoxTel = new System.Windows.Forms.TextBox();
+            this.textBoxEposta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonKaydet = new System.Windows.Forms.Button();
+            this.listBoxAd = new System.Windows.Forms.ListBox();
+            this.listBoxSoad = new System.Windows.Forms.ListBox();
+            this.listBoxTel = new System.Windows.Forms.ListBox();
+            this.listBoxEposta = new System.Windows.Forms.ListBox();
+            this.buttonListele = new System.Windows.Forms.Button();
+            this.buttonGuncelle = new System.Windows.Forms.Button();
+            this.buttonSil = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxAd
+            // 
+            this.textBoxAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxAd.Location = new System.Drawing.Point(98, 27);
+            this.textBoxAd.Name = "textBoxAd";
+            this.textBoxAd.Size = new System.Drawing.Size(201, 29);
+            this.textBoxAd.TabIndex = 0;
+            // 
+            // textBoxSoyad
+            // 
+            this.textBoxSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxSoyad.Location = new System.Drawing.Point(98, 62);
+            this.textBoxSoyad.Name = "textBoxSoyad";
+            this.textBoxSoyad.Size = new System.Drawing.Size(201, 29);
+            this.textBoxSoyad.TabIndex = 0;
+            // 
+            // textBoxTel
+            // 
+            this.textBoxTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxTel.Location = new System.Drawing.Point(98, 97);
+            this.textBoxTel.Name = "textBoxTel";
+            this.textBoxTel.Size = new System.Drawing.Size(201, 29);
+            this.textBoxTel.TabIndex = 0;
+            // 
+            // textBoxEposta
+            // 
+            this.textBoxEposta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxEposta.Location = new System.Drawing.Point(98, 132);
+            this.textBoxEposta.Name = "textBoxEposta";
+            this.textBoxEposta.Size = new System.Drawing.Size(201, 29);
+            this.textBoxEposta.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(20, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(20, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(20, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tel";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(20, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "E-Posta";
+            // 
+            // buttonKaydet
+            // 
+            this.buttonKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonKaydet.Location = new System.Drawing.Point(305, 27);
+            this.buttonKaydet.Name = "buttonKaydet";
+            this.buttonKaydet.Size = new System.Drawing.Size(205, 29);
+            this.buttonKaydet.TabIndex = 2;
+            this.buttonKaydet.Text = "Yeni Kayıt Olarak Ekle";
+            this.buttonKaydet.UseVisualStyleBackColor = true;
+            this.buttonKaydet.Click += new System.EventHandler(this.buttonKaydet_Click);
+            // 
+            // listBoxAd
+            // 
+            this.listBoxAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBoxAd.FormattingEnabled = true;
+            this.listBoxAd.ItemHeight = 20;
+            this.listBoxAd.Location = new System.Drawing.Point(16, 205);
+            this.listBoxAd.Name = "listBoxAd";
+            this.listBoxAd.Size = new System.Drawing.Size(157, 324);
+            this.listBoxAd.TabIndex = 3;
+            this.listBoxAd.SelectedIndexChanged += new System.EventHandler(this.listBoxAd_SelectedIndexChanged);
+            // 
+            // listBoxSoad
+            // 
+            this.listBoxSoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBoxSoad.FormattingEnabled = true;
+            this.listBoxSoad.ItemHeight = 20;
+            this.listBoxSoad.Location = new System.Drawing.Point(179, 205);
+            this.listBoxSoad.Name = "listBoxSoad";
+            this.listBoxSoad.Size = new System.Drawing.Size(157, 324);
+            this.listBoxSoad.TabIndex = 3;
+            this.listBoxSoad.SelectedIndexChanged += new System.EventHandler(this.listBoxAd_SelectedIndexChanged);
+            // 
+            // listBoxTel
+            // 
+            this.listBoxTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBoxTel.FormattingEnabled = true;
+            this.listBoxTel.ItemHeight = 20;
+            this.listBoxTel.Location = new System.Drawing.Point(342, 205);
+            this.listBoxTel.Name = "listBoxTel";
+            this.listBoxTel.Size = new System.Drawing.Size(157, 324);
+            this.listBoxTel.TabIndex = 3;
+            this.listBoxTel.SelectedIndexChanged += new System.EventHandler(this.listBoxAd_SelectedIndexChanged);
+            // 
+            // listBoxEposta
+            // 
+            this.listBoxEposta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBoxEposta.FormattingEnabled = true;
+            this.listBoxEposta.ItemHeight = 20;
+            this.listBoxEposta.Location = new System.Drawing.Point(505, 205);
+            this.listBoxEposta.Name = "listBoxEposta";
+            this.listBoxEposta.Size = new System.Drawing.Size(157, 324);
+            this.listBoxEposta.TabIndex = 3;
+            this.listBoxEposta.SelectedIndexChanged += new System.EventHandler(this.listBoxAd_SelectedIndexChanged);
+            // 
+            // buttonListele
+            // 
+            this.buttonListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonListele.Location = new System.Drawing.Point(578, 22);
+            this.buttonListele.Name = "buttonListele";
+            this.buttonListele.Size = new System.Drawing.Size(84, 29);
+            this.buttonListele.TabIndex = 2;
+            this.buttonListele.Text = "Listele";
+            this.buttonListele.UseVisualStyleBackColor = true;
+            this.buttonListele.Click += new System.EventHandler(this.buttonListele_Click);
+            // 
+            // buttonGuncelle
+            // 
+            this.buttonGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonGuncelle.Location = new System.Drawing.Point(305, 62);
+            this.buttonGuncelle.Name = "buttonGuncelle";
+            this.buttonGuncelle.Size = new System.Drawing.Size(205, 29);
+            this.buttonGuncelle.TabIndex = 2;
+            this.buttonGuncelle.Text = "Güncelle";
+            this.buttonGuncelle.UseVisualStyleBackColor = true;
+            this.buttonGuncelle.Click += new System.EventHandler(this.buttonGuncelle_Click);
+            // 
+            // buttonSil
+            // 
+            this.buttonSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSil.Location = new System.Drawing.Point(305, 101);
+            this.buttonSil.Name = "buttonSil";
+            this.buttonSil.Size = new System.Drawing.Size(205, 29);
+            this.buttonSil.TabIndex = 2;
+            this.buttonSil.Text = "Sil";
+            this.buttonSil.UseVisualStyleBackColor = true;
+            this.buttonSil.Click += new System.EventHandler(this.buttonSil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 552);
+            this.Controls.Add(this.listBoxEposta);
+            this.Controls.Add(this.listBoxTel);
+            this.Controls.Add(this.listBoxSoad);
+            this.Controls.Add(this.listBoxAd);
+            this.Controls.Add(this.buttonSil);
+            this.Controls.Add(this.buttonGuncelle);
+            this.Controls.Add(this.buttonListele);
+            this.Controls.Add(this.buttonKaydet);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEposta);
+            this.Controls.Add(this.textBoxTel);
+            this.Controls.Add(this.textBoxSoyad);
+            this.Controls.Add(this.textBoxAd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxAd;
+        private System.Windows.Forms.TextBox textBoxSoyad;
+        private System.Windows.Forms.TextBox textBoxTel;
+        private System.Windows.Forms.TextBox textBoxEposta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonKaydet;
+        private System.Windows.Forms.ListBox listBoxAd;
+        private System.Windows.Forms.ListBox listBoxSoad;
+        private System.Windows.Forms.ListBox listBoxTel;
+        private System.Windows.Forms.ListBox listBoxEposta;
+        private System.Windows.Forms.Button buttonListele;
+        private System.Windows.Forms.Button buttonGuncelle;
+        private System.Windows.Forms.Button buttonSil;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; // dosyalar üzerinde işlem yapmak için gerekli olan kütüphane
+namespace Dosya_Kayıt_İşlemleri
+{
+    public partial class Form1 : Form
+    {
+        string dosyaAdi = "kayitlar.ismek";
+        string[] kayitlar = new string[1];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonListele_Click(object sender, EventArgs e)
+        {
+            Array.Resize(ref kayitlar, File.ReadAllLines(dosyaAdi).Count());
+            kayitlar = File.ReadAllLines(dosyaAdi);
+            // form içerisindeki tüm nesneleri getiren bir döngü kur
+            foreach (Control kontrol in this.Controls)
+            {//her kontrol elemanını listbox mı diye kontrol et
+                if (kontrol is ListBox)
+                {//bulduğun listboxların tür dönüşümünü yaparak içerisini temizle
+                    ((ListBox)kontrol).Items.Clear();
+                }
+            }
+
+            foreach (string kayit  in kayitlar)
+            {
+                string[] alanlar = kayit.Split('#');
+                if (alanlar.Length > 1)
+                {
+                    listBoxAd.Items.Add(alanlar[0]);
+                    listBoxSoad.Items.Add(alanlar[1]);
+                    listBoxTel.Items.Add(alanlar[2]);
+                    listBoxEposta.Items.Add(alanlar[3]);
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(dosyaAdi)==false)
+            {
+                File.WriteAllText(dosyaAdi, "");
+            }
+            buttonListele.PerformClick();
+
+        }
+
+        private void buttonKaydet_Click(object sender, EventArgs e)
+        {
+            Array.Resize(ref kayitlar, kayitlar.Length + 1);
+            kayitlar[kayitlar.Length - 1] = textBoxAd.Text + "#" + textBoxSoyad.Text + "#" + textBoxTel.Text + "#" + textBoxEposta.Text; 
+            File.WriteAllLines(dosyaAdi, kayitlar);
+            buttonListele.PerformClick();
+
+        }
+
+        private void listBoxAd_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int yeniindex = ((ListBox)sender).SelectedIndex;
+            listBoxAd.SelectedIndex = yeniindex;
+            listBoxSoad.SelectedIndex = yeniindex;
+            listBoxTel.SelectedIndex = yeniindex;
+            listBoxEposta.SelectedIndex = yeniindex;
+            textBoxAd.Text = listBoxAd.Text;
+            textBoxSoyad.Text = listBoxSoad.Text;
+            textBoxTel.Text = listBoxTel.Text;
+            textBoxEposta.Text = listBoxEposta.Text;
+            //foreach (Control kontrol in this.Controls)
+            //    if (kontrol is ListBox)
+            //        ((ListBox)kontrol).SelectedIndex = yeniindex;
+                
+          
+        }
+
+        private void buttonGuncelle_Click(object sender, EventArgs e)
+        {
+            int index = listBoxAd.SelectedIndex;
+            if(index>=0)
+            {
+                kayitlar[index] = textBoxAd.Text + "#" + textBoxSoyad.Text + "#" + textBoxTel.Text + "#" + textBoxEposta.Text;
+                File.WriteAllLines(dosyaAdi, kayitlar);
+                buttonListele.PerformClick();
+                listBoxAd.SelectedIndex = index;
+            }
+
+        }
+        void sil(int index)
+        {
+            int syc = 0;
+            string[] yedek = new string[kayitlar.Length - 1];
+            for (int i = 0; i < kayitlar.Length; i++)
+            {
+                if (i!=index)
+                {
+                    yedek[syc] = kayitlar[i];
+                    syc++;
+                }
+            }
+            Array.Copy(yedek, kayitlar, yedek.Length);
+        }
+        private void buttonSil_Click(object sender, EventArgs e)
+        {
+            int index = listBoxAd.SelectedIndex;
+            if (index >= 0)
+            {
+                sil(index);
+                File.WriteAllLines(dosyaAdi, kayitlar);
+                buttonListele.PerformClick();
+            }
+        }
+    }
+}
